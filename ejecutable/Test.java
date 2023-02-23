61dd4d6b86d45afd2ad0556fceb732bd64cbcf33
Olarte18 <snheider2224656@correo.uis.edu.co>
2023-02-22 20:45:16 -0500

    code

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -8,11 +8,16 @@ public class Test
         int t;
         int n;
         int B;
-        t= Integer.parseInt(JOptionPane.showInputDialog("Digite el tiempo que duro la llamada :"));
+        int c;
+        t= Integer.parseInt(JOptionPane.showInputDialog("Digite los minutos que duro la llamada: "));
         n=t-3;
         B=300;
+        c=(n*50)+B;
         if(t<=3){JOptionPane.showMessageDialog(null, "El valor de la llamada es "+B);}
-        else{JOptionPane.showMessageDialog(null,"El valor de la llamda es "+n*;}
+        else{JOptionPane.showMessageDialog(null,"El valor de la llamda es "+c);}
+
+        //end
+        System.exit(0);
 
     }
 }
